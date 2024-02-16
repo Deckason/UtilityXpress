c6d237982c93df9a83cb1672d5184b6249cc0688
Deckason <chibuikemchidera@gmail.com>
2024-02-16 12:42:56 +0100

    Added validation to my authentication system using yup

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -7,11 +7,35 @@ import Link from 'next/link'
 import { BiEnvelope, BiLock, BiPhone, BiUser } from 'react-icons/bi'
 import { BsEye, BsEyeSlash } from 'react-icons/bs'
 
+// Form Validation with Yup
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
+
 const page = () => {
     const [passwordVisible, setPasswordVissible] = useState(false)
     const [conpasswordVisible, setConPasswordVissible] = useState(false)
-    const [disabled, setDisabled] = useState(true)
-  return (
+
+    // Validation schema
+    const validatioSchema = yup.object().shape({
+        username: yup.string().required("Username is required!"),
+        email: yup.string().required("Email is required!").email("Email format is required!"),
+        phone: yup.number().required("Mobile number is required!").positive().min(11,"Expected 11 digits for mobile number!").integer().typeError('Number is required!'),
+        password: yup.string().required("Password is required!").min(8, "Min of 8 characters required!"),
+        confirmPassword: yup.string().required("Confirm password is required!").oneOf([yup.ref("password")], "Password do not match!"),
+    })
+
+    const { register, handleSubmit, formState:{errors} } = useForm({
+        resolver: yupResolver(validatioSchema)
+    })
+
+    const registerUser = (data)=>{
+        console.log(data)
+    }
+    console.log("err", errors)
+    return (
     <div className={styles.container}>
         <div className={styles.formContainer}>
             <div className={styles.illustration}>
@@ -19,51 +43,65 @@ const page = () => {
                     src={svgIllustrator}
                     height={700}
                     width={1000}
-                    objectFit='cover'
                     alt='register-illustrator'
                 />
             </div>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit(registerUser)}>
                 <h1>Register</h1>
                 <div className={styles.formInputs}>
-                    <div className={styles.inputContainer}>
-                        <span className={styles.inputIcon}><BiUser /></span>
-                        <input type="text" placeholder='Enter Username'/>
+                    <div className={styles.inputSection}>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.inputIcon}><BiUser /></span>
+                            <input type="text" placeholder='Enter Username' {...register("username")}/>
+                        </div>
+                        <small className={styles.err}>{errors.username?.message}</small>
                     </div>
-                    <div className={styles.inputContainer}>
-                        <span className={styles.inputIcon}><BiEnvelope /></span>
-                        <input type="email" placeholder='Enter Email'/>
+                    <div className={styles.inputSection}>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.inputIcon}><BiEnvelope /></span>
+                            <input type="email" placeholder='Enter Email' {...register("email")}/>
+                        </div>
+                        <small className={styles.err}>{errors.email?.message}</small>
                     </div>
-                    <div className={styles.inputContainer}>
-                        <span className={styles.inputIcon}><BiPhone /></span>
-                        <input type="phone" placeholder='Enter Phone'/>
+                    <div className={styles.inputSection}>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.inputIcon}><BiPhone /></span>
+                            <input type={'number'} placeholder='Enter Phone' {...register("phone")}/>
+                        </div>
+                        <small className={styles.err}>{errors.phone?.message}</small>
                     </div>
-                    <div className={styles.inputContainer}>
-                        <span className={styles.inputIcon}><BiLock /></span>
-                        <input type={passwordVisible?"text":"password"} placeholder='Enter Password'/>
-                        {passwordVisible && 
-                            <span className={styles.passwordVisible} onClick={()=>setPasswordVissible(prev=>!prev)}>
-                                <BsEyeSlash />
-                            </span>}
-                        {!passwordVisible && 
-                            <span className={styles.passwordVisible} onClick={()=>setPasswordVissible(prev=>!prev)}>
-                                <BsEye />
-                            </span>}
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <span className={styles.inputIcon}><BiLock /></span>
-                        <input type={conpasswordVisible?"text":"password"} placeholder='Confirm password'/>
-                        {conpasswordVisible && 
-                            <span className={styles.passwordVisible} onClick={()=>setConPasswordVissible(prev=>!prev)}>
+                    <div className={styles.inputSection}>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.inputIcon}><BiLock /></span>
+                            <input type={passwordVisible?"text":"password"} placeholder='Enter Password' {...register("password")}/>
+                            {passwordVisible && 
+                                <span className={styles.passwordVisible} onClick={()=>setPasswordVissible(prev=>!prev)}>
                                     <BsEyeSlash />
-                            </span>}
-                        {!conpasswordVisible && 
-                            <span className={styles.passwordVisible} onClick={()=>setConPasswordVissible(prev=>!prev)}>
-                                <BsEye />
-                            </span>}
+                                </span>}
+                            {!passwordVisible && 
+                                <span className={styles.passwordVisible} onClick={()=>setPasswordVissible(prev=>!prev)}>
+                                    <BsEye />
+                                </span>}
+                        </div>
+                        <small className={styles.err}>{errors.password?.message}</small>
+                    </div>
+                    <div className={styles.inputSection}>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.inputIcon}><BiLock /></span>
+                            <input type={conpasswordVisible?"text":"password"} placeholder='Confirm password' {...register("confirmPassword")}/>
+                            {conpasswordVisible && 
+                                <span className={styles.passwordVisible} onClick={()=>setConPasswordVissible(prev=>!prev)}>
+                                        <BsEyeSlash />
+                                </span>}
+                            {!conpasswordVisible && 
+                                <span className={styles.passwordVisible} onClick={()=>setConPasswordVissible(prev=>!prev)}>
+                                    <BsEye />
+                                </span>}
+                        </div>
+                        <small className={styles.err}>{errors.confirmPassword?.message}</small>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <button disabled={disabled} className={`${disabled?styles.disabled : styles.notDisabled}`}>Register</button>
+                        <button>Register</button>
                     </div>
                     <small>Already have an Account? <Link href={"/login"}>Login</Link></small>
                 </div>
